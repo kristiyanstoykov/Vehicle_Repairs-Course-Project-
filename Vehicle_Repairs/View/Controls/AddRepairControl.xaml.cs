08afcc0c4f138fd6feb20f1084744a42277b7440
Kristiyan Stoykov <krisistoikov@gmail.com>
2024-05-18 09:24:23 +0300

    Added add repair control

---
+++ Vehicle_Repairs/View/Controls/AddRepairControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vehicle_Repairs.View.Controls
+{
+    /// <summary>
+    /// Interaction logic for AddRepairControl.xaml
+    /// </summary>
+    public partial class AddRepairControl : UserControl
+    {
+        public AddRepairControl()
+        {
+            InitializeComponent();
+        }
+
+        public void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
